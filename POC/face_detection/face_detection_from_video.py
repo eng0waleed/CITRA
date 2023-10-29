a43ca4651750e42c47780982585a1c8d07b074f2
eng0waleed <eng.0waleed@gmail.com>
2023-10-29 22:04:15 +0300

    updated

--- POC/face_detection/face_detection_from_video.py
+++ POC/face_detection/face_detection_from_video.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import time
 
 # Ensure a directory exists to store the cropped face images
 if not os.path.exists('detected_faces'):
@@ -9,11 +10,11 @@ if not os.path.exists('detected_faces'):
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Open the video
-cap = cv2.VideoCapture('faces2.mp4')
+cap = cv2.VideoCapture('http://192.168.0.106:5000/video_feed')
 
 # Face counter
 face_count = 0
-
+start_time = time.time()
 while True:
     # Read each frame from the video
     ret, frame = cap.read()
@@ -38,8 +39,11 @@ while True:
         # Save the cropped face
         filename = f"detected_faces/face_{face_count}.jpg"
         cv2.imwrite(filename, cropped_face)
-
+end_time = time.time()
 # Release the video capture object
 cap.release()
 
 cv2.destroyAllWindows()
+
+
+print("total time = ",(end_time - start_time))
