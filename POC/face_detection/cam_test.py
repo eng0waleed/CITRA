48045832328bfed648d509723762e26234e8181f
eng0waleed <eng.0waleed@gmail.com>
2023-10-29 22:05:59 +0300

    updated

--- POC/face_detection/cam_test.py
+++ POC/face_detection/cam_test.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 camera = cv2.VideoCapture(0)  # Use 0 for default camera
 
-
 def generate():
     """Video streaming generator function."""
     while True:
@@ -21,13 +20,11 @@ def generate():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_byte + b'\r\n\r\n')
 
-
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(generate(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True)
