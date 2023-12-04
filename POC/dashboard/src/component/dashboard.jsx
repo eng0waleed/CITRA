e2b4a1b326213845dbbbbbde49f89366c694fbb6
ahmed <ahmed@ahmed.co>
2023-12-05 02:04:51 +0300

    changes

--- POC/dashboard/src/component/dashboard.jsx
+++ POC/dashboard/src/component/dashboard.jsx
@@ -6,65 +6,46 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const CITRADashboard = () => {
-const [chartData, setChartData] = useState({
-labels: [],
-datasets: [{
-label: 'Emotion Distribution',
-data: [],
-backgroundColor: [
-'rgba(255, 99, 132, 0.2)',
-'rgba(54, 162, 235, 0.2)',
-// ... add more colors as needed
-],
-borderColor: [
-'rgba(255, 99, 132, 1)',
-'rgba(54, 162, 235, 1)',
-// ... add more border colors as needed
-],
-borderWidth: 1
-}]
-});
+  const [chartData, setChartData] = useState({ datasets: [] });
 
-useEffect(() => {
-fetch('https://citra-eva2ckcawq-uc.a.run.app/values') // Replace with your API endpoint
-.then(response => response.json())
-.then(data => {
-console.log()
-const emotions = Object.keys(data);
-const percentages = Object.values(data);
+  useEffect(() => {
+    fetch('https://citra-eva2ckcawq-uc.a.run.app/values')
+      .then(response => response.json())
+      .then(data => {
+        setChartData({
+          labels: Object.keys(data),
+          datasets: [{
+            label: 'Emotion Distribution',
+            data: Object.values(data),
+            backgroundColor: [
+              '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40'
+            ],
+            borderColor: [
+              '#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40'
+            ],
+            borderWidth: 1
+          }]
+        });
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
-setChartData(prevData => ({
-...prevData,
-labels: emotions,
-datasets: [{
-...prevData.datasets[0],
-data: percentages
-}]
-}));
-})
-.catch(error => {
-console.error('Error fetching data:', error);
-// Handle error state as needed
-});
-}, []);
-
-return (
-<Grid container spacing={2}>
-<Grid item xs={12}>
-<Paper elevation={3} style={{ padding: '20px' }}>
-<Typography variant="h4" gutterBottom>
-CITRA Dashboard
-</Typography>
-{chartData.labels.length > 0 ? (
-<Pie data={chartData} />
-) : (
-<Typography variant="subtitle1">Loading data...</Typography>
-)}
-</Paper>
-</Grid>
-</Grid>
-);
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Paper elevation={3} style={{ padding: '20px' }}>
+          <Typography variant="h4" gutterBottom>
+            CITRA Dashboard
+          </Typography>
+          {chartData.datasets.length > 0 ? (
+            <Pie data={chartData} />
+          ) : (
+            <Typography variant="subtitle1">Loading data...</Typography>
+          )}
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default CITRADashboard;
-
